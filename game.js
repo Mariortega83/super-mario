6dcc6a92abafb8228c5e6e9082ea09ef67f437b8
Mario Ortega <mortper0408@ieszaidinvergeles.org>
2024-07-24 14:00:42 +0200

    primeras imagenes

--- game.js
+++ game.js
@@ -4,7 +4,7 @@ const config = {
     type : Phaser.AUTO,
     width: 256,
     height: 244,
-    backgroundColor: '#fff',
+    backgroundColor: '#049cd8',
     parent: 'game',
     scene:{
         preload, // se ejecuta para cargar recrusos
@@ -16,11 +16,36 @@ const config = {
 new Phaser.Game(config)
 
 function preload() {
+    this.load.image(
+        'cloud1',
+        'assets/scenery/overworld/cloud1.png'
+    )
+
+    this.load.image(
+        'floorbricks',
+        'assets/scenery/overworld/floorbricks.png'
+    )
+
+    this.load.spritesheet(
+        'mario',
+        'assets/entities/mario.png',
+        { frameWidth : 18, frameHeight : 16 } 
+    )
     
 }
 
 function create() {
-    console.log('create')
+    //image(x,y,id-del-asset)
+    this.add.image(100, 50, 'cloud1')
+        .setOrigin(0, 0)
+        .setScale(0.15)
+
+    this.add.tileSprite(0, config.height -32, config.width, 32,
+         'floorbricks' )
+         .setOrigin(0, 0)
+
+    this.add.sprite(50, 210, 'mario')
+        .setOrigin(0,1)
 }
 
 function update() {
