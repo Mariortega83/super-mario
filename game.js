c2288cc219f367d5e6587a843e8b8a087dd27359
Mario Ortega <mortper0408@ieszaidinvergeles.org>
2024-07-26 00:00:23 +0200

    primeras fisicas

--- game.js
+++ game.js
@@ -6,6 +6,13 @@ const config = {
     height: 244,
     backgroundColor: '#049cd8',
     parent: 'game',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300},
+            debug: false
+        }
+    },
     scene:{
         preload, // se ejecuta para cargar recrusos
         create, // se ejecuta cuando el juego comienza
@@ -37,26 +44,48 @@ function preload() {
 }
 
 function create() {
+    
     //image(x,y,id-del-asset)
     this.add.image(100, 50, 'cloud1')
         .setOrigin(0, 0)
         .setScale(0.15)
 
-    this.add.tileSprite(0, config.height -32, config.width, 32,
-         'floorbricks' )
-         .setOrigin(0, 0)
+    
+    this.floor = this.physics.add.staticGroup()
 
-    this.mario = this.add.sprite(50, 210, 'mario')
-        .setOrigin(0,1)
+    this.floor
+        .create(0, config.height -32, 'floorbricks')
+        .setOrigin(0,0)
 
+    this.floor
+        .create(150, config.height -32, 'floorbricks')
+        .setOrigin(0,0)
+
+
+    this.physics.add.collider(this.mario, this.floor)
+    
+    this.mario = this.physics.add.sprite(50, 210, 'mario')
+        .sertOrigin(0,1)
+        .setGravity(1)
+        
     this.anims.create({
         key: 'mario-walk',
         frames: this.anims.generateFrameNumbers(
             'mario',
-            { start: 3, end: 1 }
+            { start: 1, end: 3 }
          ),
         frameRate: 12,
         repeat: -1
+    })
+
+    this.anims.create({
+        key: 'mario-idle',
+        frames: [{key: 'mario',frame: '0'}]
+    })
+
+    this.anims.create({
+        key: 'mario-jump',
+        frames: [{key: 'mario', frame: 5}]
     })
 
     this.keys = this.input.keyboard.createCursorKeys()
@@ -67,13 +96,18 @@ function update() {
     if(this.keys.left.isDown){
         this.mario.anims.play('mario-walk', true)
         this.mario.x -= 2     
+        this.mario.flipX = true
     }else if (this.keys.right.isDown){
         this.mario.anims.play('mario-walk', true)
         this.mario.x += 2
-        
+        this.mario.flipX = false
     }else {
-        this.mario.anims.stop()
-        this.mario.setFrame(0)
+        this.mario.anims.play('mario-idle', true)
     }
     
+
+    if(this.keys.up.isDown){
+        this.mario.y -= 5
+        this.mario.anims.play('mario-jump', true)
+    }
 }
